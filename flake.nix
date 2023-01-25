dd1aaed4c325a51bfba3dcd92ffd0e39f6124813
Amanse <amansetiarjp@gmail.com>
2023-01-25 20:32:53 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustup rust-analyzer];
+	  shellHook = ''
+	  	export PATH="$PATH:/home/me/.cargo/bin"
+	  '';
+        };
+      }
+    );
+}
