09a9691afffb960e3b2717e874ddf548d8a1a2df
Amanse <amansetiarjp@gmail.com>
2023-01-25 21:57:03 +0530

    Add run subcommand
    run subcommand either takes the id of the game and runs the game, but in case no id is provided, it will give an interactive fuzzy searchable list of games

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
+use clap::{Parser, Subcommand, Args};
 use runner::Runner;
 mod runner;
 
-fn main() {
-    let id = std::env::args().nth(1).expect("no id given");
-    let id: usize = id.parse().unwrap(); 
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    run: RunCommand,
+}
 
+#[derive(Subcommand)]
+enum RunCommand {
+    Run(Run),
+}
+
+#[derive(Args)]
+struct Run {
+    id: Option<usize>,
+}
+
+fn main() {
+    let cli = Cli::parse();
     let runner = Runner::new();
-    runner.run_game(id);
-        
+
+    match &cli.run {
+        RunCommand::Run(id) => {
+            match id.id {
+                Some(v) => runner.run_game(v),
+                None => println!("No id was given, interactive selection will be here")
+            }
+        }
+    }
 }
