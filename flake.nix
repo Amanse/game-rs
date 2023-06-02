4bb24768afbbb2caa98f7a6adb13fff87897a4cb
AmanSe <amansetiarjp@gmail.com>
2023-06-02 14:28:29 +0530

    Use crane instead of naesrk

--- flake.nix
+++ flake.nix
@@ -1,31 +1,43 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
+    # naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    crane,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = (import nixpkgs) {
           inherit system;
         };
-
-        naersk' = pkgs.callPackage naersk {};
-
-      in rec {
+        #
+        # naersk' = pkgs.callPackage naersk {};
+        craneLib = crane.lib.${system};
+      in {
         # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+          # Add extra inputs here or any other derivation settings
+          # doCheck = true;
+          # buildInputs = [];
+          # nativeBuildInputs = [];
         };
 
         # For `nix develop`:
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustup rust-analyzer];
-	  shellHook = ''
-	  	export PATH="$PATH:/home/me/.cargo/bin"
-	  '';
+          nativeBuildInputs = with pkgs; [rustup rust-analyzer];
+          shellHook = ''
+            export PATH="$PATH:/home/me/.cargo/bin"
+          '';
         };
       }
     );
