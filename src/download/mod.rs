8281acc4afb03d3e16652297b18ca37d93d4547a
AmanSe <amansetiarjp@gmail.com>
2024-02-03 14:41:14 +0530

    Major download refactor+ulgwl downloading
    cleared up download module in a big way, is very flexible now, can be
    used to download multiple runners in future too

--- src/download/mod.rs
+++ src/download/mod.rs
@@ -1,12 +1,25 @@
-pub mod proton;
-pub mod ulgwl;
+mod util;
 
 use crate::DownloadOptions;
-use eyre::Result;
+use eyre::{eyre, Result};
 
 pub fn download(what: &DownloadOptions) -> Result<()> {
     match what {
-        DownloadOptions::Proton => proton::download(),
-        DownloadOptions::ULGWL => Ok(()),
+        DownloadOptions::Proton => util::download_and_extract(&get_proton_url()?),
+        DownloadOptions::ULGWL => util::download_and_extract(
+            "https://api.github.com/repos/Open-Wine-Components/ULWGL-launcher/zipball",
+        ),
     }
+}
+
+fn get_proton_url() -> Result<String> {
+    let resp: serde_json::Value =
+        ureq::get("https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/latest")
+            .call()?
+            .into_json()?;
+
+    Ok(resp["assets"][1]["browser_download_url"]
+        .as_str()
+        .ok_or(eyre!("Could not get download link from github"))?
+        .to_string())
 }
